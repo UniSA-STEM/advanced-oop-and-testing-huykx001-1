4f2981583bc8152483968fc1b3862a1843d43e89
huykx001 <huykx001@mymail.unisa.edu.au>
2025-11-21 02:34:14 +1030

    Created placeholder classes for enclosure

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,26 @@
-'''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
+"""
+File: enclosure.py
+Description: This module represents the enclosure class.
+Author: Kim Xuyen Huynh
 ID: 110100110
-Username: bizvy001
+Username: Huykx001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+"""
+
+from animal import Animal
+from categories import Mammal, Reptile, Bird
+
+class Aquatic:
+    pass
+
+class Savannah:
+    pass
+
+class Forest:
+    pass
+
+class deserts:
+    pass
+
+class Arctic:
+    pass
