135fd227311cd4d8c076351e10fcf993bc3cf10f
huykx001 <huykx001@mymail.unisa.edu.au>
2025-11-22 04:18:29 +1030

    Updated the attribute and added methods to implement coding

--- enclosure.py
+++ enclosure.py
@@ -10,22 +10,33 @@ This is my own work as defined by the University's Academic Integrity Policy.
 from animal import Animal
 from categories import Mammal, Reptile, Bird
 
+
 class Enclosure:
     """
     Constructor for the class Enclosure with private attributes such as
     size, environment, cleanliness level, and animals.
     """
+
     def __init__(self, size, environment_type, cleanliness_level):
         self.__size = size
-        self.__environment_type = environment_type
+        self.__environment_type = {"Savannah": None,
+                                   "Desert": None,
+                                   "Forest": None,
+                                   "Aquatic": None}
         self.__cleanliness_level = cleanliness_level
         self.__animals = []
 
+    def get_environment_type(self):
+        pass
+
+    def add_animal(self, animal):
+        pass
+
+    def remove_animal(self, animal):
+        pass
 
     def __str__(self):
         """
         returns the current status
         """
         pass
-
-
