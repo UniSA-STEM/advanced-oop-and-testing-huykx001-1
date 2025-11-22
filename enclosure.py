ba1ab5600041309bec3866a75d428210491892b9
huykx001 <huykx001@mymail.unisa.edu.au>
2025-11-22 23:35:42 +1030

    Created an method to check the animal's compatibility with the habitat

--- enclosure.py
+++ enclosure.py
@@ -8,7 +8,7 @@ This is my own work as defined by the University's Academic Integrity Policy.
 """
 
 from animal import Animal
-from categories import Mammal, Reptile, Bird
+from categories import Mammal, Reptile, Bird, Fish
 
 
 class Enclosure:
@@ -19,15 +19,44 @@ class Enclosure:
 
     def __init__(self, size, environment_type, cleanliness_level):
         self.__size = size
-        self.__environment_type = {"Savannah": None,
-                                   "Desert": None,
-                                   "Forest": None,
-                                   "Aquatic": None}
+        self.__environment_type = environment_type
         self.__cleanliness_level = cleanliness_level
         self.__animals = []
 
-    def get_environment_type(self):
-        pass
+    def check_compatibility(self, animal):
+        """
+        Function checks if the animal is suited for the habitat or not.
+        """
+        # checks if the type matches the specific habitat
+        if self.__environment_type == "savannah":
+            if isinstance(animal, Mammal):
+                # Animal is compatible with habitat.
+                return True
+            else:
+                # Animal is not compatible with habitat.
+                print(f"{animal.get_name()} is not compatible with the savannah habitat.")
+                return False
+
+        if self.__environment_type == "desert":
+            if isinstance(animal, Reptile):
+                return True
+            else:
+                print(f"{animal.get_name()} is not compatible with the desert habitat.")
+                return False
+
+        if self.__environment_type == "forest":
+            if isinstance(animal, Bird):
+                return True
+            else:
+                print(f"{animal.get_name()} is not compatible with the forest habitat.")
+                return False
+
+        if self.__environment_type == "aquatic":
+            if isinstance(animal, Fish):
+                return True
+            else:
+                print(f"{animal.get_name()} is not compatible with the aquatic habitat.")
+                return False
 
     def add_animal(self, animal):
         pass
