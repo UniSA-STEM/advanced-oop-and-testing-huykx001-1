1173eaea58de7ea6ad6d65675368e7fb75dadd6b
huykx001 <huykx001@mymail.unisa.edu.au>
2025-11-24 05:33:37 +1030

    updated the staff subclass's methods

--- staff.py
+++ staff.py
@@ -7,11 +7,6 @@ Username: Huykx001
 This is my own work as defined by the University's Academic Integrity Policy.
 """
 
-# Note: remember to add data validation, testing
-
-from categories import Mammal, Reptile, Bird, Fish
-from systemHealthRecord import SystemHealthRecord
-
 
 class Staff:
     """
@@ -47,54 +42,96 @@ class Zookeeper(Staff):
         """
         assigns the zookeeper to a specific enclosure
         """
-        self.get_assigned_enclosure().append(enclosure)
-        print(f"{self.get_name()} is now assigned to the enclosure.")
+        if enclosure.get_animals():
+            # Assigns the zookeeper to the specific enclosure.
+            self.get_assigned_enclosure().append(enclosure.get_environment_type())
+            print(f"{self.get_name()} is now assigned to the {enclosure.get_environment_type()} enclosure.")
+        else:
+            # No animals assigned in an enclosure.
+            print(f"There are currently no animals in the enclosure for {self.get_name()} to look after")
 
-    def feed_animal(self, animal, hunger_level, current_hunger):
+    def feed_animal(self, animal, hunger_level, current_hunger, enclosure):
         """
         feeds the animal depending on what level hunger it's at.
         """
-        # Animal cannot eat if it is already full.
-        if animal.get_max_hunger() <= hunger_level:
-            print(f"{animal.get_name()} is too full to consume more {animal.get_dietary()}!")
+        # Checks zookeeper is assigned to the enclosure.
+        if enclosure.get_environment_type() in self.get_assigned_enclosure():
+            # Animal cannot eat if it is already full.
+            if animal.get_max_hunger() <= hunger_level:
+                print(f"{animal.get_name()} is too full to consume more {animal.get_dietary()}!")
+            else:
+                # Animal consumes food, lowering its hunger level.
+                hunger_level -= current_hunger
+                print(f"{animal.get_name()} just consumed some delicious {animal.get_dietary()} yum!")
+                print(f"Current hunger level: {current_hunger}")
         else:
-            # Animal consumes food, lowering its hunger level.
-            hunger_level -= current_hunger
-            print(f"{animal.get_name()} just consumed some delicious {animal.get_dietary()} yum!")
-            print(f"Current hunger level: {current_hunger}")
+            # Zookeeper is not assigned to any enclosures.
+            print(f"{self.get_name()} is not assigned to any enclosures!")
 
     def clean_enclosure(self, enclosure):
         """
         cleans the enclosure depending on what cleanliness level it's at.
         """
-        # The enclosure is now untidy.
-        if enclosure.get_cleanliness_level() == "dirty":
-            self.__clean_percentage += 50
-            print(f"cleaning enclosure...cleanliness now is at {self.__clean_percentage}%")
-        # The enclosure is now fully cleaned.
-        elif enclosure.get_cleanliness_level() == "untidy":
-            self.__clean_percentage += 100
-            print(f"cleaning enclosure...cleanliness now is at {self.__clean_percentage}% fully cleaned!")
-        elif enclosure.get_cleanliness_level() == "clean":
-            print(f"The enclosure is already cleaned!")
+        # Checks if Zookeeper is assigned to the enclosure.
+        if enclosure.get_environment_type() in self.get_assigned_enclosure():
+            # The enclosure is now untidy.
+            if enclosure.get_cleanliness_level() == "dirty":
+                self.__clean_percentage += 50
+                print(f"cleaning enclosure...cleanliness now is at {self.__clean_percentage}%")
+            # The enclosure is now fully cleaned.
+            elif enclosure.get_cleanliness_level() == "untidy":
+                self.__clean_percentage += 100
+                print(f"cleaning enclosure...cleanliness now is at {self.__clean_percentage}% fully cleaned!")
+            elif enclosure.get_cleanliness_level() == "clean":
+                print(f"The enclosure is already cleaned!")
+        else:
+            # Zookeeper is not assigned to any enclosures.
+            print(f"{self.get_name()} is not assigned to any enclosure")
 
 
 class Veterinarian(Staff):
     """
     Constructor the child class inherited by the Staff class.
     """
+
     def __init__(self, name):
         super().__init__(name)
-        self.__under_treatment = False
+        self.__under_treatment = None
+        self.__vet_enclosure = []
 
     # Getter for the private attribute.
     def get_under_treatment(self):
         return self.__under_treatment
 
-    def add_record(self, animal, record):
+    def assign_enclosure(self, enclosure):
+        """
+        Assigns vet to specific enclosure.
+        """
+        if enclosure.get_animals():
+            # Assigns the Vet into the specific enclosure.
+            self.__vet_enclosure.append(enclosure.get_environment_type())
+            print(f"{self.get_name()} is now assigned to the {enclosure.get_environment_type()} enclosure")
+
+    def add_record(self, animal, record, enclosure):
         """
         record health issues added to the sick/injured animal.
         """
-        print(f"Added the record {record.get_name()} to the {animal.get_name()}")
-        self.__under_treatment = True
+        # Checks if the vet is assigned to an enclosure.
+        if enclosure.get_environment_type() in self.__vet_enclosure:
+            print(f"{self.get_name()} has added the record {record.get_name()} to {animal.get_name()}")
+            # Animal will now undergo treatment.
+            self.__under_treatment = True
+        else:
+            # Vet is not assigned to any enclosures.
+            print(f"{self.get_name()} cannot add record due to not being assigned to any enclosures!")
 
+    def check_record(self):
+        if self.__under_treatment:
+            return f"under treatment!"
+        else:
+            return f"healthy!"
+
+    def __str__(self):
+        return (f"\n---HEATH STATUS---\n"
+                f"name: \n"
+                f"status: {self.check_record()}")
